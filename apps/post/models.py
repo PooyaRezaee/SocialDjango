2f3d700cd8513b6cb8bc6413e66b6c3ab0d3a4e1
PooyaRezaee <pooya.rezaee.official@gmail.com>
2023-03-28 19:15:39 +0430

    Added Tests For Post Models

--- apps/post/models.py
+++ apps/post/models.py
@@ -5,6 +5,7 @@ from taggit.managers import TaggableManager
 class Post(models.Model):
     title = models.CharField(max_length=64)
     text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='posts')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -16,18 +17,18 @@ class Post(models.Model):
 
 
 class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='likes')
 
     def __str__(self):
-        return self.user
+        return str(self.user)
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    replied_to = models.ForeignKey('self', on_delete=models.SET_NULL,blank=True,null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='comments')
+    replied_to = models.ForeignKey('self', on_delete=models.SET_NULL,blank=True,null=True,related_name='reply')
     text = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='comments')
     creataed = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- apps/post/tests.py
+++ apps/post/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
+from apps.accounts.models import User
+from .models import Post,Comment,Like
 
-# Create your tests here.
+class PostModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.tags = ['test1','test2','test3']
+        cls.user = User.objects.create_user(username='testuser',email="test@t.tt", password='testpass')
+        cls.post = Post.objects.create(title='Test Post', text='Test content', author=cls.user,tags=cls.tags)
+
+    def test_post_str(self):
+        self.assertEquals(str(self.post), 'Test Post')
+
+    def test_tags_posts(self):
+        for tag in self.tags:
+            self.assertTrue(tag in self.post.tags)
+
+
+class LikeModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username='testuser',email="test@t.tt", password='testpass')
+        cls.post = Post.objects.create(title='Test Post', text='Test content', author=cls.user)
+        cls.like = Like.objects.create(user=cls.user, post=cls.post)
+
+
+    def test_like_str(self):
+        self.assertEquals(str(self.like), str(self.user))
+
+    def test_like_post(self):
+        self.assertEqual(self.post.likes.count(), 1)
+
+    def test_dislike_post(self):
+        self.like.delete()
+        self.assertEqual(self.post.likes.count(), 0)
+
+
+
+
+class CommentModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username='testuser',email="test@t.tt", password='testpass')
+        cls.post = Post.objects.create(title='Test Post', text='Test content', author=cls.user)
+        cls.comment = Comment.objects.create(post=cls.post, text='Test comment', user=cls.user)
+
+    def test_comment_str(self):
+        self.assertEquals(str(self.comment), str(self.user))
+
+    def test_comment_for_post(self):
+        self.assertEqual(self.post.comments.count(), 1)
+        self.assertEqual(self.post.comments.first(), self.comment)
+
+    def test_reply_to_comment(self):
+        comment_rep = Comment.objects.create(post=self.post, text='Test reply', user=self.user,replied_to=self.comment)
+
+        self.assertEqual(self.comment.reply.count(), 1)
+        self.assertEqual(self.comment.reply.first(), comment_rep)
