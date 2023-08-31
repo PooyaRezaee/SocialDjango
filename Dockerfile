6975ab1e118834562b6db26d1cb840c1dd4aa934
PooyaRezaee <pooya.rezaee.official@gmail.com>
2023-08-31 13:10:17 +0330

    Initilize Dockerize project (It needs to be completed)

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.11-slim-bullseye
+
+ENV PIP_DISABLE_PIP_VERSION_CHECK 1
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /src
+
+COPY ./requirements.txt .
+RUN pip install -r requirements.txt
+COPY . .
--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+load_dotenv()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.dev')
 
 application = get_wsgi_application()
